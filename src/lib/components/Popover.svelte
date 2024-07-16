5583da29a523fabef8ccf69f590bd9047a258e40
Matt King <matthewpaulking@outlook.com>
2024-07-16 07:48:11 -0700

    feat: Popover composability

--- src/lib/components/Popover.svelte
+++ src/lib/components/Popover.svelte
@@ -1,10 +1,6 @@
 <script lang="ts">
 	import type { Snippet } from 'svelte';
 
-	function handleClick() {
-		popoverOpen = !popoverOpen;
-		onclick();
-	}
 	function onfocusout(e: FocusEvent) {
 		if (!containerEl?.contains(e.relatedTarget as Node | null)) {
 			popoverOpen = false;
@@ -19,24 +15,17 @@
 	let {
 		popoverOpen = $bindable(false),
 		class: classes,
-		onclick = () => null,
-		btnContent,
 		children
 	}: {
 		popoverOpen: boolean;
-		class: string;
-		onclick?: () => void;
-		btnContent: Snippet;
+		class?: string;
 		children: Snippet;
 	} = $props();
 
-	let containerEl: HTMLButtonElement | undefined = $state();
+	let containerEl: HTMLDivElement | undefined = $state();
 </script>
 
 <svelte:window {onfocusout} {onkeydown} />
-<div class="relative">
-	<button onclick={handleClick} bind:this={containerEl} class="relative {classes}">
-		{@render btnContent()}
-	</button>
+<div bind:this={containerEl} class="relative p-0! m-0! {classes}">
 	{@render children()}
 </div>
--- src/routes/+layout.svelte
+++ src/routes/+layout.svelte
@@ -4,11 +4,10 @@
 	import { Toast, setToaster } from '$lib/components/Toaster';
 	import { Breadcrumb, List, Avatar, Popover } from '$lib/components';
 	import { slide } from 'svelte/transition';
-	let { children } = $props();
 	setToaster();
 	setModalStore();
 	let userMenuOpen = $state(false);
-	$inspect(userMenuOpen);
+	let { children } = $props();
 </script>
 
 <Toast />
@@ -16,20 +15,20 @@
 <div class="flex flex-col overflow-y-auto overflow-x-hidden h-screen w-screen">
 	<div class="sticky w-full flex h-fit py-2 px-4 bg-surface-100-800-token place-items-center">
 		<Breadcrumb class="w-full" />
-		<Popover bind:popoverOpen={userMenuOpen} class="h-9">
-			{#snippet btnContent()}
+		<Popover class="h-9" bind:popoverOpen={userMenuOpen}>
+			<button onclick={() => (userMenuOpen = !userMenuOpen)}>
 				<Avatar
 					initials="MK"
 					class="{userMenuOpen
 						? 'scale-110 ring-1 ring-secondary-500-400-token'
 						: ''} bg-secondary-400-500-token h-9"
 				/>
-			{/snippet}
+			</button>
 			{#if userMenuOpen}
 				<List transition={slide} class="absolute right-0 top-full" label="Hello World">
 					<button class="btn btn-sm variant-ghost">1</button>
 					<button class="btn btn-sm variant-ghost">2</button>
-					<button class="btn variant-ghost">3</button>
+					<button class="btn btn-sm variant-ghost">3</button>
 				</List>
 			{/if}
 		</Popover>
