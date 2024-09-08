98469447679f01c6ef1f2a32a62084d38117b134
Matt King <matthewpaulking@outlook.com>
2024-09-08 12:50:33 -0700

    feat: card for main page

--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -1,9 +1,9 @@
 <script lang="ts">
 	import { browser } from '$app/environment';
 	import { setModeCurrent } from '@skeletonlabs/skeleton';
-	import type { PageData } from './$types';
 	import { NeoButton, NeoAnchorArrow } from './lib/components/neo-brutalism';
 	import NeoCard from './lib/components/neo-brutalism/NeoCard.svelte';
+	import type { PageData } from './$types';
 	let { data }: { data: PageData } = $props();
 	if (browser) {
 		setModeCurrent(true);
@@ -18,15 +18,21 @@
 	<div class="absolute h-screen w-screen -z-10 bg-img"></div>
 	<div class="flex justify-around container h-full place-items-center">
 		<section class="flex flex-col">
-			<h1 class="h1 font-bold">Matthew King</h1>
-			<a class="anchor text-secondary-800" href="mailto:matthewpaulking@outlook.com"
-				>matthewpaulking@outlook.com</a
-			>
-			<div class="flex gap-x-4 place-items-center mt-6">
-				<NeoButton class="variant-filled-tertiary w-fit">Resume</NeoButton>
-				<NeoButton class="bg-surface-300-600-token w-fit">Github</NeoButton>
-				<NeoButton class="bg-surface-300-600-token w-fit">LinkedIn</NeoButton>
-			</div>
+			<NeoCard>
+				<h1 class="h1 font-bold">Matthew King</h1>
+				<a class="anchor text-secondary-800" href="mailto:matthewpaulking@outlook.com"
+					>matthewpaulking@outlook.com</a
+				>
+				<div class="flex gap-x-4 place-items-center mt-6">
+					<NeoButton class="variant-filled-tertiary w-fit">Resume</NeoButton>
+					<NeoButton class="bg-primary-400-500-token w-fit flex gap-x-2">
+						<img src="/Github.png" alt="github" class="h-6 w-6" />Github</NeoButton
+					>
+					<NeoButton class="bg-primary-400-500-token w-fit flex gap-x-2">
+						<img src="/LinkedIn.png" alt="github" class="h-6 w-6" />LinkedIn</NeoButton
+					>
+				</div>
+			</NeoCard>
 		</section>
 
 		<section class="flex flex-col w-fit items-end gap-y-8">
