18c0e2f26b026a210deee8aad3e9a4ca27fc220b
Matt King <matthewpaulking@outlook.com>
2024-09-08 17:03:06 -0700

    feat: better background

--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -1,9 +1,10 @@
 <script lang="ts">
 	import { browser } from '$app/environment';
 	import { setModeCurrent } from '@skeletonlabs/skeleton';
-	import { NeoButton, NeoAnchorArrow } from './lib/components/neo-brutalism';
+	import { NeoButton, NeoSpark } from './lib/components/neo-brutalism';
 	import NeoCard from './lib/components/neo-brutalism/NeoCard.svelte';
 	import type { PageData } from './$types';
+	import NeoBadge from './lib/components/neo-brutalism/NeoBadge.svelte';
 	let { data }: { data: PageData } = $props();
 	if (browser) {
 		setModeCurrent(true);
@@ -19,6 +20,9 @@
 	<div class="flex justify-around container h-full place-items-center">
 		<section class="flex flex-col">
 			<NeoCard>
+				{#snippet cardChildren()}
+					<NeoSpark class="absolute -top-6 stroke-black size-12 -right-6 z-20 fill-tertiary-500" />
+				{/snippet}
 				<h1 class="h1 font-bold">Matthew King</h1>
 				<a class="anchor text-secondary-800" href="mailto:matthewpaulking@outlook.com"
 					>matthewpaulking@outlook.com</a
@@ -38,27 +42,45 @@
 		<section class="flex flex-col w-fit items-end gap-y-8">
 			<h2 class="h2 font-bold">Sample Websites</h2>
 			<NeoCard class="bg-secondary-400-500-token">
+				{#snippet cardChildren()}
+					<NeoBadge class="absolute -top-4 stroke-black size-12 -right-4 z-20 fill-tertiary-500" />
+				{/snippet}
 				<h3 class="h3 font-bold">Spresso</h3>
-				<span>Non-profit Programming Courses</span>
-				<NeoAnchorArrow href="/rec-charity" class="bg-tertiary-300-600-token self-end"
-				></NeoAnchorArrow>
+				<span class="mb-4">Non-profit Programming Courses</span>
+				<a href="/rec-charity" class="w-fit group self-end"
+					><NeoButton class="bg-tertiary-500-400-token">
+						<div class="flex gap-x-2 place-items-center">
+							<span>View</span>
+							<img
+								src="/ArrowRight.png"
+								alt="arrow"
+								class="group-hover:translate-x-2 transition-transform"
+							/>
+						</div>
+					</NeoButton></a
+				>
 			</NeoCard>
 			<NeoCard class="bg-primary-400-500-token">
+				{#snippet cardChildren()}
+					<NeoBadge class="absolute -top-4 stroke-black size-12 -right-4 z-20 fill-secondary-500" />
+				{/snippet}
 				<h3 class="h3 font-bold">The Bull</h3>
-				<span>Restaurant</span>
-				<NeoAnchorArrow href="/restaurant" class="bg-tertiary-300-600-token self-end"
-				></NeoAnchorArrow>
+				<span class="mb-4">Restaurant</span>
+				<a href="/restaurant" class="w-fit group self-end"
+					><NeoButton class="bg-secondary-500-400-token">
+						<div class="flex gap-x-2 place-items-center">
+							<span>View</span>
+							<img
+								src="/ArrowRight.png"
+								alt="arrow"
+								class="group-hover:translate-x-2 transition-transform"
+							/>
+						</div>
+					</NeoButton></a
+				>
+				<!-- <NeoAnchorArrow href="/restaurant" class="bg-tertiary-300-600-token self-end" -->
+				<!-- ></NeoAnchorArrow> -->
 			</NeoCard>
 		</section>
 	</div>
 </div>
-
-<style>
-	.bg-img {
-		background-image: url('/brutal-bg.png') !important;
-		background-attachment: fixed !important;
-		background-position: center !important;
-		background-repeat: no-repeat !important;
-		background-size: cover !important;
-	}
-</style>
--- src/routes/lib/components/neo-brutalism/NeoCard.svelte
+++ src/routes/lib/components/neo-brutalism/NeoCard.svelte
@@ -1,11 +1,18 @@
 <script lang="ts">
 	import type { Snippet } from 'svelte';
 
-	let { class: classes, children }: { class?: string; children: Snippet } = $props();
+	let {
+		class: classes,
+		cardChildren,
+		children
+	}: { class?: string; cardChildren?: Snippet; children: Snippet } = $props();
 </script>
 
 <div class="flex flex-col w-full relative">
-	<div class="absolute bg-black rounded-token w-full h-full top-3 -right-3"></div>
+	{#if cardChildren}
+		{@render cardChildren()}
+	{/if}
+	<div class="absolute bg-black rounded-md w-full h-full top-3 -right-3"></div>
 	<div
 		class="card z-10 p-4 flex flex-col w-full {classes} shadow-lg shadow-gray-700 rounded-token border-black border-2"
 	>
